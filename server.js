985eabf4532b0218eb98c4cb6fb2968d59886251
Hammad Khan <77hammadkhan@gmail.com>
2026-05-29 21:21:08 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/water", require("./routes/waterRoutes"));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Running");
+});
